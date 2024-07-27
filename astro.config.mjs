98a8ec5a53f8ff1d65e3a1d8a8a624a7ae656325
Tiana Clemons <tiana.clemons@duke.edu>
2024-07-26 22:31:34 -0400

    new build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://tiananc.github.io',
+  base: 'tiananc.github.io',
 });
